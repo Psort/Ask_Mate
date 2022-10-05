4caeaac7c1cd7c0897ad0c2de6bd920e9fb07f61
RafalGontarski <gontarskirafal@gmail.com>
2022-10-05 14:11:56 +0200

    own practice

--- connection.py
+++ connection.py
@@ -1,14 +1,3 @@
-import os
 
-
-def add_file(fileitem):
-    try:
-        filename = os.path.basename(fileitem.filename)
-        filename = 'static/photo/'+filename
-        open(filename, 'wb').write(fileitem.file.read())
-    except:
-        filename = "aaaaaaaaaaaaaNULL"
-    finally:
-        return filename[13:len(filename)]
 
 
