e5a2c55840dc40847e4fa63923946ca344eb6042
Psort <skowronpiotr.00@gmail.com>
2022-10-05 14:09:13 +0200

    repair answer accept

--- database_common.py
+++ database_common.py
@@ -4,10 +4,6 @@ import os
 import psycopg2
 import psycopg2.extras
 
-os.environ['PSQL_USER_NAME'] = 'postgres'
-os.environ['PSQL_PASSWORD'] = 'Jula12345'
-os.environ['PSQL_HOST'] = 'localhost'
-os.environ['PSQL_DB_NAME'] = 'ask_mate'
 
 
 def get_connection_string():
